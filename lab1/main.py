80374661fff516f78c23836fd1985d0bd1aee3d0
üsâme_aw <osamasaedawad@gmail.com>
2023-10-24 13:10:35 +0300

    Initial Commit for MediaPipe stuff

---
+++ lab1/main.py
@@ -0,0 +1,43 @@
+import cv2
+
+from mediapipe_controller import MP_Controller
+import draw_landmarks
+
+
+def main():
+    # start video capture through webcam
+    GAME_MODE = 2
+    cap = cv2.VideoCapture(0)
+
+    test_landmarker = MP_Controller(GAME_MODE)
+
+    while True:
+        # pull frame
+        ret, frame = cap.read()
+        # mirror frame
+        frame = cv2.flip(frame, 1)
+
+        test_landmarker.detect_async(frame, GAME_MODE)
+
+        # draw landmarks on frame
+        frame = draw_landmarks.draw_landmarks_on_hand(
+            frame, test_landmarker.hand_result
+        )
+
+        frame = draw_landmarks.draw_landmarks_on_face(
+            frame, test_landmarker.face_result
+        )
+
+        # display image
+        cv2.imshow("frame", frame)
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
+
+    # release everything
+    test_landmarker.close()
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
---
+++ lab1/mediapipe_controller.py
@@ -0,0 +1,82 @@
+import time
+import mediapipe as mp
+import numpy as np
+
+
+class MP_Controller:
+    def __init__(self, mode=1):
+        self.hand_result = mp.tasks.vision.HandLandmarkerResult
+        self.hand_landmarker = mp.tasks.vision.HandLandmarker
+        self.createHandLandmarker()
+
+        if mode == 2:
+            self.face_result = mp.tasks.vision.FaceLandmarkerResult
+            self.face_landmarker = mp.tasks.vision.FaceLandmarker
+            self.createFaceLandmarker()
+
+    def createHandLandmarker(self):
+        # callback function
+        def update_result(
+            hand_result: mp.tasks.vision.HandLandmarkerResult,
+            output_image: mp.Image,
+            timestamp_ms: int,
+        ):
+            self.hand_result = hand_result
+
+        options_hands = mp.tasks.vision.HandLandmarkerOptions(
+            base_options=mp.tasks.BaseOptions(
+                model_asset_path="hand_landmarker.task"
+            ),  # path to model
+            running_mode=mp.tasks.vision.RunningMode.LIVE_STREAM,  # running on a live stream
+            num_hands=1,  # track both hands
+            min_hand_detection_confidence=0.5,  # lower than value to get predictions more often
+            min_hand_presence_confidence=0.5,  # lower than value to get predictions more often
+            min_tracking_confidence=0.5,  # lower than value to get predictions more often
+            result_callback=update_result,
+        )
+
+        # initialize landmarker
+        self.hand_landmarker = self.hand_landmarker.create_from_options(options_hands)
+
+    def createFaceLandmarker(self):
+        # callback function
+        def update_result(
+            face_result: mp.tasks.vision.FaceLandmarkerResult,
+            output_image: mp.Image,
+            timestamp_ms: int,
+        ):
+            self.face_result = face_result
+
+        # HandLandmarkerOptions (details here: https://developers.google.com/mediapipe/solutions/vision/hand_landmarker/python#live-stream)
+        options_face = mp.tasks.vision.FaceLandmarkerOptions(
+            base_options=mp.tasks.BaseOptions(
+                model_asset_path="face_landmarker.task"
+            ),  # path to model
+            running_mode=mp.tasks.vision.RunningMode.LIVE_STREAM,  # running on a live stream
+            num_faces=1,
+            min_face_detection_confidence=0.5,  # lower than value to get predictions more often
+            min_face_presence_confidence=0.5,  # lower than value to get predictions more often
+            min_tracking_confidence=0.5,  # lower than value to get predictions more often
+            result_callback=update_result,
+        )
+
+        # initialize landmarker
+        self.face_landmarker = self.face_landmarker.create_from_options(options_face)
+
+    def detect_async(self, frame, mode):
+        # convert np frame to mp image
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+        # detect landmarks
+        self.hand_landmarker.detect_async(
+            image=mp_image, timestamp_ms=int(time.time() * 1000)
+        )
+
+        if mode == 2:
+            self.face_landmarker.detect_async(
+                image=mp_image, timestamp_ms=int(time.time() * 1000)
+            )
+
+    def close(self):
+        # close landmarker
+        self.hand_landmarker.close()
+        self.face_landmarker.close()
